d361672b125df370e87cbf75ccd6afbd58a068cc
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2020-10-21 13:18:43 -0400

    first push

--- routes/contactInfo.js
+++ routes/contactInfo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contactInfo', { title: '' });
+  res.render('contactInfo', { title: 'Contact Info' });
 });
 
 module.exports = router;
